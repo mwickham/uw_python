3cd34fcfedfa9e431f6302b2f7b7833bfb33015f
Michele Wickham <mishwik@571758.no-reply.drupal.org>
2012-01-10 20:30:49 -0800

    Update to client

--- echo/echo_client.py
+++ echo/echo_client.py
@@ -6,25 +6,37 @@ echo client, usage:
 Both host and port are optional, defaults: localhost 50000
 host must be present if you want to provide port
 """
-
-import socket 
+import socket
 import sys
+
+def send_chat():
+    s = socket.socket(socket.AF_INET,
+                      socket.SOCK_STREAM)
+    s.connect((host,port))
+    s.send(chat)
+
+    data = s.recv(size)
+    s.close()
+    print 'from (%s,%s) %s' % (host, port, data)
+
 
 host = 'localhost' 
 port = 50000 
-size = 1024 
+size = 1024
+chat = ''
 
 nargs = len(sys.argv)
 if nargs > 1:
-    host = sys.argv[1]
+    chat = sys.argv[1]
 if nargs > 2:
     port = int(sys.argv[2])
+if nargs > 3:
+    host = sys.argv[3]
 
-s = socket.socket(socket.AF_INET, 
-                  socket.SOCK_STREAM) 
-s.connect((host,port)) 
-s.send('Hello, world') 
-data = s.recv(size) 
-s.close() 
-print 'from (%s,%s) %s' % (host, port, data)
+if (chat):
+    send_chat()
+else:
+    print 'Please enter a message'
+    chat = raw_input('--> ')
+    send_chat()
 
